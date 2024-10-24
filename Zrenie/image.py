81a4cd728b931be0b155cd2aba66ce5f30e40e74
Makar Kamardin <vsyakofazno@gmail.com>
2024-10-24 20:02:46 +0300

    task about trafic lights

--- Zrenie/image.py
+++ Zrenie/image.py
@@ -1,7 +1,18 @@
+import cv2
 from PIL import Image
+from PIL import ImageFilter
 import numpy as np
-image = Image.open('cat.png')
-image.rotate(45,expand=True).show()
+
+image = cv2.imread('PythonProjects/Zrenie/5.png')
+
+hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+hsv_image[:, :, 0] = np.clip(hsv_image[:, :, 0] + 30, 0, 180) 
+hsv_image[:, :, 1] = np.clip(hsv_image[:, :, 1] * 1.5, 0, 255) 
+hsv_image[:, :, 2] = np.clip(hsv_image[:, :, 2] + 50, 0, 255)
+cv2.imshow('PythonProjects/Zrenie/5.png', hsv_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
 
 
 
